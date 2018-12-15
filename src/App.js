4ce8b36bd7d613f8bbb1c556f267d68a9b50ba31
ali1129js <ali1129js@gmail.com>
2018-12-15 18:49:43 +0100

    async await

--- src/App.js
+++ src/App.js
@@ -2,19 +2,25 @@
  * @Author: Ali
  * @Date:   2018-12-15T13:27:09+01:00
  * @Last modified by:   Ali
- * @Last modified time: 2018-12-15T16:57:05+01:00
+ * @Last modified time: 2018-12-15T18:49:18+01:00
  */
 import React, { Component } from 'react'
 import Form from './Components/Form'
 import './App.css'
 
+const API_KEY = "d6e5954a2b386a35dd8b85fbe821fa80"
+
 class App extends Component {
 
-
-  //new way to write methods with arrow-function -no need to bind it to this-
-  getRecipe = (e) => {
+  /*new way to write methods with arrow-function
+    -no need to bind it to this-
+  */
+  getRecipe = async (e) => {
     e.preventDefault()
-    console.log("works")
+    const recipeName = e.target.elements.recipeName.value
+    const api_call = await fetch(`https://www.food2fork.com/api/search?key=${API_KEY}&q=chicken%20breast&page=2`)
+    const data = await api_call.json()
+    console.log(recipeName)
   }
   render() {
     return (
--- src/Components/Form.js
+++ src/Components/Form.js
@@ -2,14 +2,15 @@
  * @Author: Ali
  * @Date:   2018-12-15T16:44:03+01:00
  * @Last modified by:   Ali
- * @Last modified time: 2018-12-15T16:52:13+01:00
+ * @Last modified time: 2018-12-15T17:57:41+01:00
  */
 import React from 'react'
 
 //a stateless functional Component
 const Form = props => (
   <form onSubmit={props.getRecipe}>
-   <input type="text" />
+    {/* using string refs for now, must change later*/}
+   <input type="text" name="recipeName"/>
    <button> Search </button>
   </form>
 )
