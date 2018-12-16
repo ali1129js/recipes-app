175a8787809be21a009be9dfd2d0360d952b3fd3
ali1129js <ali1129js@gmail.com>
2018-12-16 11:46:21 +0100

    api call works

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@
  * @Author: Ali
  * @Date:   2018-12-15T13:27:09+01:00
  * @Last modified by:   Ali
- * @Last modified time: 2018-12-15T18:49:18+01:00
+ * @Last modified time: 2018-12-16T11:45:41+01:00
  */
 import React, { Component } from 'react'
 import Form from './Components/Form'
@@ -13,14 +13,19 @@ const API_KEY = "d6e5954a2b386a35dd8b85fbe821fa80"
 class App extends Component {
 
   /*new way to write methods with arrow-function
-    -no need to bind it to this-
+    -no need to bind it to (this)-
   */
   getRecipe = async (e) => {
     e.preventDefault()
+    //we grap the input text from the element "recipeName"
     const recipeName = e.target.elements.recipeName.value
-    const api_call = await fetch(`https://www.food2fork.com/api/search?key=${API_KEY}&q=chicken%20breast&page=2`)
+    // added https://cors-anywhere.herokuapp.com/ because the site won't allow local servers to access their data
+    const api_call = await fetch(`https://cors-anywhere.herokuapp.com/https://www.food2fork.com/api/search?key=${API_KEY}&q=chicken%20breast&page=2`)
+    //we call .json() on the api_call instead of using
+    // JSON.parse/stringfy
+    // and we store the results in this data constant
     const data = await api_call.json()
-    console.log(recipeName)
+    console.log(data)
   }
   render() {
     return (
