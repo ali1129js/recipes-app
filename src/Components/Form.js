74c5a765a7e682fb37ca21b74d7ae47ba6a82789
ali1129js <ali1129js@gmail.com>
2018-12-18 08:45:25 +0100

    styling and bootstrap

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -2,16 +2,19 @@
  * @Author: Ali
  * @Date:   2018-12-15T16:44:03+01:00
  * @Last modified by:   Ali
- * @Last modified time: 2018-12-15T17:57:41+01:00
+ * @Last modified time: 2018-12-18T08:35:31+01:00
  */
 import React from 'react'
 
 //a stateless functional Component
 const Form = props => (
-  <form onSubmit={props.getRecipe}>
-    {/* using string refs for now, must change later*/}
-   <input type="text" name="recipeName"/>
-   <button> Search </button>
+  <form onSubmit={props.getRecipe} style={{ marginBottom:"2rem" }}>
+    {/* using name attribute for now, must change later*/}
+    <input
+      className="form__input"
+      type="text"
+      name="recipeName"/>
+    <button className="form__button"> Search </button>
   </form>
 )
 export default Form
--- src/Components/Recipes.js
+++ src/Components/Recipes.js
@@ -2,21 +2,32 @@
  * @Author: Ali
  * @Date:   2018-12-16T14:09:47+01:00
  * @Last modified by:   Ali
- * @Last modified time: 2018-12-16T14:34:45+01:00
+ * @Last modified time: 2018-12-18T08:43:36+01:00
  */
 import React from 'react'
 
 const Recipes = props => (
-  <div>
-    {props.recipes.map( recipe => {
-      return (
-        <div key={recipe.recipe_id}>
-          <img src={recipe.image_url} alt={recipe.title} />
-          <p> { recipe.title } </p>
-        </div>
-      )
-    }
-    )}
+  <div className="container">
+    <div className="row">
+      {props.recipes.map( recipe => {
+        return (
+          <div className="col-md-4">
+            <div className="recipes__box">
+              <div key={recipe.recipe_id}>
+                <img
+                  className="recipe__box-img"
+                  src={recipe.image_url}
+                  alt={recipe.title} />
+                  <div className="recipe__text">
+                    <h5> { recipe.title } </h5>
+                  </div>
+              </div>
+            </div>
+          </div>
+        )
+      }
+      )}
+    </div>
   </div>
 )
 export default Recipes
