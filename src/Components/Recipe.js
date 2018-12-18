fef784c040a755a2723c979692740938222ee5d9
ali1129js <ali1129js@gmail.com>
2018-12-18 11:00:20 +0100

    added React-router

---
+++ src/Components/Recipe.js
@@ -0,0 +1,12 @@
+/**
+ * @Author: Ali
+ * @Date:   2018-12-18T10:52:56+01:00
+ * @Last modified by:   Ali
+ * @Last modified time: 2018-12-18T10:55:59+01:00
+ */
+import React from 'react'
+
+const Recipe = () => (
+ <div className=""> Recipe</div>  
+)
+export default Recipe
--- src/index.js
+++ src/index.js
@@ -2,16 +2,16 @@
  * @Author: Ali
  * @Date:   2018-12-16T14:09:02+01:00
  * @Last modified by:   Ali
- * @Last modified time: 2018-12-16T14:36:08+01:00
+ * @Last modified time: 2018-12-18T10:55:23+01:00
  */
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Router from './Components/Router';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Router />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
