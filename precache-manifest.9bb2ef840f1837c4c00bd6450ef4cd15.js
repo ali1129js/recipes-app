e8374e1d48ae858f820ec47b5b5f7a22b41c7293
ali1129js <ali1129js@gmail.com>
2019-01-12 12:21:52 +0100

    Updates

---
+++ precache-manifest.9bb2ef840f1837c4c00bd6450ef4cd15.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "eccb75b530ef64e7b363",
+    "url": "/recipes-app/static/js/runtime~main.eccb75b5.js"
+  },
+  {
+    "revision": "2ded9b2e77d5fe813d06",
+    "url": "/recipes-app/static/js/main.2ded9b2e.chunk.js"
+  },
+  {
+    "revision": "3f1394363a22428d8d9e",
+    "url": "/recipes-app/static/js/1.3f139436.chunk.js"
+  },
+  {
+    "revision": "2ded9b2e77d5fe813d06",
+    "url": "/recipes-app/static/css/main.84c1d6a3.chunk.css"
+  },
+  {
+    "revision": "3f1394363a22428d8d9e",
+    "url": "/recipes-app/static/css/1.147d6329.chunk.css"
+  },
+  {
+    "revision": "022b094729cb8ca5807e77f351835d83",
+    "url": "/recipes-app/index.html"
+  }
+];
